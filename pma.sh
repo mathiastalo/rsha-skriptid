aff67aff7c9656b8ce370cac8968db86809d999b
Mathias Talo <mathias.talo@khk.ee>
2020-04-28 15:29:22 +0300

    Pma paigaldamine

---
+++ pma.sh
@@ -0,0 +1,16 @@
+#Phpmyadmin paigaldus
+
+#Kontrollib, kas tarkvara on juba olemas või vajab installeerimist
+PMA=$(dpkg-query -W -f="${status}" phpmyadmin 2>/dev/null | grep -c "ok installed")
+
+#Kui PMA väärtus = null siis ta instaleerib ja paigaldab teenuse, ning annab sellest ka teada
+if [ $PMA -eq 0 ]; then
+echo "Phpmyadmin paigaldamine"
+apt install phpmyadmin
+echo "Phpmyadmin on paigaldatud"
+
+#Kui PMA väärtus = 1, siis annab sellest teada
+echo "Phpmyadmin on juba paigaldatud"
+fi
+
+#The End
